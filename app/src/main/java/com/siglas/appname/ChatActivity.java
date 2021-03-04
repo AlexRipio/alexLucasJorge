45ed960c3498f1b8d313d35c7f4027cb25fbd68b
Alex <alejandroripio@gmail.com>
2021-03-04 21:15:42 +0100

    Nueva actualizacion

--- app/src/main/java/com/siglas/appname/ChatActivity.java
+++ app/src/main/java/com/siglas/appname/ChatActivity.java
@@ -3,12 +3,32 @@ package com.siglas.appname;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.cenkgun.chatbar.ChatBarView;
 
 public class ChatActivity extends AppCompatActivity {
+
+    TextView textView;
+    ChatBarView chatBarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        textView = (TextView)findViewById(R.id.textView);
+        chatBarView = (ChatBarView)findViewById(R.id.ChatBar);
+
+        //Setup
+        chatBarView.setMessageBoxHint("Enter your message");
+        chatBarView.setSendClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(chatBarView.getMessageText());
+            }
+        });
+
     }
 }
--- app/src/main/java/com/siglas/appname/Registration.java
+++ app/src/main/java/com/siglas/appname/Registration.java
@@ -7,30 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Login extends AppCompatActivity {
-    Button Login;
+public class Registration extends AppCompatActivity {
 
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration2);
 
-        Login = (Button) findViewById(R.id.btnlogin);
-
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Login.this, ChatActivity.class);
-                startActivity(intent);
-            }
-        });
-        Button register;
         register = (Button) findViewById(R.id.btnregister);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this, Registration.class);
+                Intent intent = new Intent(Registration.this, Login.class);
                 startActivity(intent);
             }
         });
